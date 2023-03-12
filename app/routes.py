1ec58c767768b89c2360ac57851e6d561247161a
fagnerfjass <fagnerfjas@gmail.com>
2023-03-12 10:59:58 -0300

    Readme.md

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,22 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for
 
 @app.route('/')
 @app.route('/index')
-def index():
+def index():    
     return render_template('index.html')
+
+def guest(nome=""):
+    if(nome == 'admin'):
+        return redirect(url_for('admin', ))
+    else:
+        return "Bem vindo {}".format(nome)
+
+def admin():
+    return 'Administrador'
+
+app.add_url_rule('/guest/', 'guest')
+app.add_url_rule('/guest/<nome>', 'guest', guest)
+
+
+app.add_url_rule('/admin/', 'admin', admin)
